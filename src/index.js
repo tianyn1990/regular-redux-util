be72446c631a5015d90c80e95c91c528127da356
田亚楠 <hztianyanan@HIH-L-3827.local>
2018-04-28 19:35:56 +0800

    babel-cli webpack-cli

---
+++ src/index.js
@@ -0,0 +1,31 @@
+let Immutable = require('immutable');
+let u = require('./tool/util');
+let createStore = require('./tool/create-store');
+let createReducer = require('./tool/create-reducer');
+let createAction = require('./tool/create-action');
+let connect = require('./connector/connect');
+let StoreProvider = require('./connector/store-provider');
+let thunkMiddleware = require('./middlewares/thunk');
+let promiseMiddleware = require('./middlewares/promise');
+module.exports = {
+    Immutable,
+    fromJS: Immutable.fromJS,
+    toJS: u.toJS,
+
+    // 工具
+    createStore: createStore,
+    createReducer: createReducer,
+    createAction: createAction,
+
+    // 容器组件的连接器
+    connect: connect,
+
+    // 添加 <StoreProvider> 容器组件，通过 SomeComponent.use 来使用
+    StoreProvider: StoreProvider,
+
+    // 中间件
+    middleware: {
+        thunk: thunkMiddleware,
+        promise: promiseMiddleware
+    }
+};
